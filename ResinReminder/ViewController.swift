18320e6e224e3daa6a0420281529aa992cd3167b
Andrew Saeyang <a09saeyang@gmail.com>
2022-03-20 18:56:58 -0700

    first commit

---
+++ ResinReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResinReminder
+//
+//  Created by Andrew Saeyang on 3/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
